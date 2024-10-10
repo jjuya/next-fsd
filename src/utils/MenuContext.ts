f2af737b439801f3ed1ca5533d3e31fcc872acd6
jjuya <404_jjuya@kakao.com>
2024-10-10 17:50:07 +0900

    init fsd for next project

--- src/utils/MenuContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext, Dispatch, SetStateAction } from 'react'
-
-type MenuContextType = { menuKey: string; setMenuKey: Dispatch<SetStateAction<string>> }
-
-export const MenuContext = createContext<MenuContextType | null>(null)
--- src/utils/mediaQuery.ts
+++
@@ -1,9 +0,0 @@
-import { generateMedia } from 'styled-media-query'
-
-// using less then
-export const antMedia = generateMedia({
-    xl: '1199px',
-    lg: '991px',
-    md: '767px',
-    sm: '575px'
-})
